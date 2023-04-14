b47b175682ce6a479e510b361133c5c40b58fb7c
levanmamuladze <levanmamoulatze@gmail.com>
2023-04-14 17:41:20 +0000

    timer done

---
+++ src/js/component/block.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Block = ({time}) => {
+    return(
+    <div className="bg-dark text-white p-5 rounded-circle">
+      {time}
+    </div>
+);
+};
+
+export default Block;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,54 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, { useState, useEffect } from "react";
+import Block from "./block.jsx";
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+
+  const [timer, setTimer] = useState(0);
+  const [isPlay, setIsPlay] = useState(true);
+  const [showButtons, setShowButtons] = useState(true);
+  const buttonPlayPause = () => {
+    setIsPlay(!isPlay);
+  };
+  const again = () => {
+    setTimer(0);
+  };
+  useEffect(() => {
+    let interval=null;
+    if (isPlay) {
+      interval = setInterval(() => {
+        setTimer((timer) => timer + 1);
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [isPlay]);
+  return (
+    <div className="container">
+      <div className="d-flex justify-content-center pt-2">
+        <button onClick={() => setShowButtons(!showButtons)}>
+          {!showButtons ? "Show" : "Hide"}
+        </button>
+	  </div>
+      <div className=" d-flex flex-wrap justify-content-center align-items-center pt-2">
+       <i class=" display-1 fa-solid fa-clock pr-2"></i>
+       <Block  time={Math.floor(timer / 100000) % 10}/>
+       <Block  time={Math.floor(timer / 10000) % 10}/>
+       <Block  time={Math.floor(timer / 1000) % 10}/>
+       <Block  time={Math.floor(timer / 100) % 10}/>
+       <Block  time={Math.floor(timer / 10) % 10}/>
+       <Block  time={Math.floor(timer) % 10}/>
+      </div>
+      {showButtons && (
+        <div className="d-flex justify-content-center pt-2 ">
+          <button
+            className={`btn btn-${isPlay ? "danger" : "success"} mx-2`}
+            onClick={buttonPlayPause}
+          >
+            {isPlay ? "Pause" : "Play"}
+          </button>
+          <button onClick={again}>Restart</button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
